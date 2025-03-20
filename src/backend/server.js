8cc82c980a59f3cd3a53f1536de68dc955568009
mungtung <shamus360@googlemail.com>
2025-03-20 12:09:39 +0000

    added POST route for posting comments. can accept multipart form data

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const path = require('path');
 const database = require('./database'); //use this to call the database.js functions
+const multer = require('multer');
+
+const upload = multer(); 
 
 
 const app = express();
 app.use(express.static(path.join(__dirname, '../static')))
-const port = 80;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(upload.none()); 
+
+
+const PORT = 80;
 
 
 
-app.post('api/comment', (req, res) => {
-    //retrieve form data
+app.post('/api/comments', (req, res) => {
+
+    /*
+        retrieve form data from request
+        request would be from a form with method POST and action "api/comments"
+
+        <form action="/api/comments" method="post">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+        </form> 
+    */ 
+
+    const data = Object.assign({}, req.body);
+    console.log(data);
 
     //sanitize form data
 
@@ -18,18 +41,20 @@ app.post('api/comment', (req, res) => {
     database.addPostToDatabase();
 
     //return post as http response - JSON
+    res.status(200).send();
 });
 
-app.get('api/comments', (req,res) => {
+
+app.get('/api/comments', (req,res) => {
     //retrieve all posts and return as JSON
     database.readPostsFromDatabase();
 });
 
-app.patch('api/comments', (req, res) => {
+app.patch('/api/comments', (req, res) => {
     database.updatePostInDatabase();
 });
 
-app.delete('api/comment', (req, res) => {
+app.delete('/api/comment', (req, res) => {
     database.deletePostFromDatabase();
 });
 
@@ -40,8 +65,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../static/index.html'));
 });
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Lowen Baban listening on PORT ${PORT}`);
 });
 
 
